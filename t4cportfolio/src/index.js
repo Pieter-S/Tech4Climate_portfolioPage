037025cc08a3936b6b9c53a822ee4636d762ac82
Pieter-S <58326506+Pieter-S@users.noreply.github.com>
2022-01-03 20:08:11 +0100

    Logo en navbar toegevoegd.

--- t4cportfolio/src/index.js
+++ t4cportfolio/src/index.js
@@ -1,4 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
+import './index.css';
+// import './components/Test';
 
+
+function tick() {
+  const element = (
+    <div className="row">
+      <h1 className="col-md-8">Hello, world!</h1>
+      {/* <h2 className="col-md-4">It is {new Date().toLocaleTimeString()}.</h2> */}
+    </div>
+  );
+
+ReactDOM.render(
+element,
+    document.getElementById('root')
+  );
+}
+setInterval(tick, 1000);
